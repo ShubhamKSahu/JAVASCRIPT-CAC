c4d4ea9f1e85c313864f8834b8404d81b388e81c
ShubhamKSahu <161279798+ShubhamKSahu@users.noreply.github.com>
2025-01-23 17:23:09 +0000

    variables lec 1

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,12 @@
+const myAge = 23
+let myName = "Ram"
+var myCity = "Mumbai"
+myEmail= "ghwghw@55636"
+//myAge = 24
+myName= "Rahul"
+myCity = "Bengaluru"
+console.log(myAge);
+console.log(myName);
+console.log(myCity);
+console.table([myAge, myCity, myName, myEmail])
+console.log(typeof[myEmail])
